d6c84ca1a23c1cb243b998e5cadb91ab2b39f77d
Josh Colvin <joshuacolvin@Joshuas-MacBook-Pro.local>
2021-03-07 02:46:38 -0600

    Added proper headers to allow coors

--- load-table.php
+++ load-table.php
@@ -1,7 +1,12 @@
 <?php
     header("Content-Type: application/json; charset=UTF-8");
+    header("Access-Control-Allow-Origin","*");
+    header("Access-Control-Allow-Methods","POST, GET");
+    header("Access-Control-Allow-Methods","Content-Type, Content-Length, Origin, Accept, X-Requested-With");
+
 	$file = fopen("contacts.json","r") or die("The file cannot be found");
 	$fileString = fread($file,filesize("contacts.json"));
 	fclose($file);
+	
 	echo $fileString;
 ?>
