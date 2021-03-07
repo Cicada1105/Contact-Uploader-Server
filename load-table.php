af20dcbd63e5603f8c7fd0c00f3beb82d904d8cb
Josh Colvin <joshuacolvin@Joshuas-MacBook-Pro.local>
2021-03-07 16:02:05 -0600

    Included headers to handle CORS

--- load-table.php
+++ load-table.php
@@ -1,8 +1,8 @@
 <?php
+    header("Access-Control-Allow-Origin: *");
     header("Content-Type: application/json; charset=UTF-8");
-    header("Access-Control-Allow-Origin","*");
-    header("Access-Control-Allow-Methods","POST, GET");
-    header("Access-Control-Allow-Methods","Content-Type, Content-Length, Origin, Accept, X-Requested-With");
+    header("Access-Control-Allow-Methods: POST, GET");
+    header("Access-Control-Allow-Headers: Content-Type, Content-Length, Origin, Accept, X-Requested-With");
 
 	$file = fopen("contacts.json","r") or die("The file cannot be found");
 	$fileString = fread($file,filesize("contacts.json"));
